c2a95e4ade4a1eea7832b7faaaa71a4bfd498276
xiaojumao <46224362+HanweiWu@users.noreply.github.com>
2021-03-21 22:46:07 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -879,14 +879,22 @@ Download_SSR(){
 }
 Service_SSR(){
 	if [[ ${release} = "centos" ]]; then
-		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ssrmu_centos -O /etc/init.d/ssrmu; then
+#whwset if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ssrmu_centos -O /etc/init.d/ssrmu; then
+#whwset
+		rm -rf ssrmu_debian
+#whwset
+		mv "ssrmu_centos" " /etc/init.d/ssrmu"; then
 			echo -e "${Error} ShadowsocksR服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/ssrmu
 		chkconfig --add ssrmu
 		chkconfig ssrmu on
 	else
-		if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ssrmu_debian -O /etc/init.d/ssrmu; then
+#whwset if ! wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/service/ssrmu_debian -O /etc/init.d/ssrmu; then
+#whwset
+		rm -rf ssrmu_centos
+#whwset
+		mv "ssrmu_debian" " /etc/init.d/ssrmu"; then
 			echo -e "${Error} ShadowsocksR服务 管理脚本下载失败 !" && exit 1
 		fi
 		chmod +x /etc/init.d/ssrmu
